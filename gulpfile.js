6a75053c9b1935f375f341b9b7351b92a9f2a110
Sergei <dynkoser@gmail.com>
2017-09-19 16:11:40 -0700

    save modified files

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,30 @@ var gulp = require('gulp');
 var concatCss = require('gulp-concat-css');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
+var path = "C:/wamp/www/Ratchet-Cloud-PHP-Test-Task";
 
 gulp.task('styles', function() {
     return gulp.src('_scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(concatCss("site.css"))
-        .pipe(gulp.dest('C:/wamp/www/Ratchet-Cloud-PHP-Test-Task/css/'));
+        .pipe(gulp.dest(path+'/css/'));
 });
 
 gulp.task('html', function() {
     return gulp.src('./*.html')
-        .pipe(gulp.dest('C:/wamp/www/Ratchet-Cloud-PHP-Test-Task/'));
+        .pipe(gulp.dest(path+'/'));
 });
 
 gulp.task('php', function() {
     return gulp.src('./*.php')
         
-        .pipe(gulp.dest('C:/wamp/www/Ratchet-Cloud-PHP-Test-Task/'));
+        .pipe(gulp.dest(path+'/'));
 });
 
 gulp.task('js', function() {
     return gulp.src('js/**/*.js')
         .pipe(concat("site.js"))
-        .pipe(gulp.dest('C:/wamp/www/Ratchet-Cloud-PHP-Test-Task/js/'));
+        .pipe(gulp.dest(path+'/js/'));
 });
 
 //Watch task
